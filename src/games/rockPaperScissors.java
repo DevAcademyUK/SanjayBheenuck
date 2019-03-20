017651d73be07e7e4dacc5c1e1186530a0529ca4
SanjayBheenuck <48724324+SanjayBheenuck@users.noreply.github.com>
2019-03-20 12:31:43 +0000

    updating games file

--- src/games/rockPaperScissors.java
+++ src/games/rockPaperScissors.java
@@ -1,65 +1,66 @@
 package games;
 
 import java.util.Random;
-import java.util.Scanner;
+import java.util.Scanner;   //Here we
+//Here we import the utils, scanner and random, used to read data and generate numbers for the computer "AI"
 
 public class rockPaperScissors {
 
-    String playerName;
-    String playerMove;
+    String playerName; //Here we set up instance variables used throughout the program.
+    String playerMove; //These hold the values for player and computer moves. And score.
     String computerMove;
     int score;
     Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args){
-        rockPaperScissors rps = new rockPaperScissors();
+        rockPaperScissors rps = new rockPaperScissors();  //Main method calling the method initialise game.
         rps.initialiseGame();
     }
 
 
     private void initialiseGame(){
-        System.out.println("Enter your name: ");
-        playerName = myScanner.nextLine();
-        playGame();
+        System.out.println("Enter your name: ");  //Scanner caller reading nextLine for player name.
+        playerName = myScanner.nextLine();  //Held in string playername
+        playGame();  //Calling method playGame
     }
 
     private void playGame(){
-        System.out.println("Enter your move: ");
-        playerMove = myScanner.nextLine();
+        System.out.println("Enter your move: ");  //Prompt to enter move, scanner call reading input
+        playerMove = myScanner.nextLine();   //Which populates player move variable
         System.out.println(playerName + " has selected " + playerMove + " as their move");
         computerMove = generateComputerMove();
         String result = getWinner(playerMove, computerMove);
-        if(result.equalsIgnoreCase("Win")){
+        if(result.equalsIgnoreCase("Win")){  //If, else loop handling win conditions and +1 to score.
             System.out.println("Congratulations, you won this round!");
             score++;
         }
-        else if(result.equalsIgnoreCase("Lose")){
+        else if(result.equalsIgnoreCase("Lose")){ //If else loop handling lose conditions and -1 to score.
             System.out.println("You lost this round!");
             score--;
         }
         else {
-            System.out.println("This round was a draw!");
+            System.out.println("This round was a draw!"); //Else printing draw message
         }
 
         if(score < 3 && score > -3){
-            playGame();
+            playGame();  //If loop reading score variable to determine if a winning or loosing score is achieved.
         }
         else if(score == 3){
-            System.out.println("Winner Winner!!");
+            System.out.println("Winner Winner!!"); //Else if score is 3 you've won.
         }
         else if(score == -3){
-            System.out.println("You Looooooosssseeeee!");
+            System.out.println("You Looooooosssseeeee!"); //Else if score is -3 you lose.
         }
 
     }
 
     private String generateComputerMove() {
-        Random r = new Random();
+        Random r = new Random();  //Method calls randomizer to generate number for computer move.
         String move = "";
-        int i = r.nextInt(3);
+        int i = r.nextInt(3); //populating local variable i to hold computer move number.
         switch(i){
             case 0:
-                move = "Rock";
+                move = "Rock";  //Based on 0-3 cases handle rock, paper scissors for computer move.
                 break;
 
             case 1:
@@ -75,8 +76,8 @@ public class rockPaperScissors {
 
     private String getWinner(String playerMove, String computerMove){
         String outcome = "";
-        if(playerMove.equalsIgnoreCase("rock")){
-            if(computerMove.equalsIgnoreCase("rock")){
+        if(playerMove.equalsIgnoreCase("rock")){   //Method handles all possible combinations for the game.
+            if(computerMove.equalsIgnoreCase("rock")){ //Uses if else loops to generate win/lose draw outcome.
                 outcome = "Draw";
             }
             else if(computerMove.equalsIgnoreCase("paper")){
@@ -86,8 +87,8 @@ public class rockPaperScissors {
                 outcome = "Win";
             }
         }
-        if(playerMove.equalsIgnoreCase("paper")){
-            if(computerMove.equalsIgnoreCase("rock")){
+        if(playerMove.equalsIgnoreCase("paper")){ //If else loop continues to calculate combinations
+            if(computerMove.equalsIgnoreCase("rock")){  //Ie. Rock beats paper
                 outcome = "Win";
             }
             else if(computerMove.equalsIgnoreCase("paper")){
@@ -97,8 +98,8 @@ public class rockPaperScissors {
                 outcome = "Lose";
             }
         }
-        if(playerMove.equalsIgnoreCase("scissors")){
-            if(computerMove.equalsIgnoreCase("rock")){
+        if(playerMove.equalsIgnoreCase("scissors")){   //If else loop continues to work on game combinations.
+            if(computerMove.equalsIgnoreCase("rock")){ //Ie. Rock beats scissors
                 outcome = "Lose";
             }
             else if(computerMove.equalsIgnoreCase("paper")){
